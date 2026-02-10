c7f4598a3b4f132e98af677f120481ec1ca6b754
so0hub <so0mile02@gmail.com>
2026-02-10 17:55:39 +0900

    Merge remote-tracking branch 'origin/유환빈'
    # Conflicts:
    #	src/unitshare/controller/ProductController.java

--- src/unitshare/controller/ProductController.java
+++ src/unitshare/controller/ProductController.java
@@ -1,7 +1,8 @@
 package unitshare.controller;
 
 public class ProductController {
-    // 박소영
-    //유환빈
-    //kim
+    // 싱글톤 생성
+    private ProductController(){}
+    private static final ProductController instance = new ProductController();
+    public static ProductController getInstance(){return instance;}
 }
--- src/unitshare/controller/UserController.java
+++ src/unitshare/controller/UserController.java
@@ -1,4 +1,9 @@
 package unitshare.controller;
 
-public class UserController{
+public class UserController {
+    // 싱글톤 생성
+    private UserController() {
+    }
+    private  static final UserController instance = new UserController();
+    public static UserController getInstance() {return instance;}
 }
