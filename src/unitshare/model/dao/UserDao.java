08b85a39162651b4b035d2380063f1570c92fb55
ys06 <yongseongg00@gmail.com>
2026-02-10 18:04:30 +0900

    userdao+productdao 싱글톤 생성 및 db연동

--- src/unitshare/model/dao/UserDao.java
+++ src/unitshare/model/dao/UserDao.java
@@ -1,4 +1,32 @@
 package unitshare.model.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class UserDao {
-}
+    //싱글톤 생성
+    private UserDao(){}
+    public static final UserDao instance = new UserDao();
+    public static UserDao getInstance() {
+        return instance;
+    }
+
+
+    //db연동
+    private String url="jdbc:mysql://localhost:3306/unishare";
+    private String user = "root";
+    private String pw = "1234";
+    private Connection conn;
+
+    private void connect() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, user, pw);
+            System.out.println("연동성공");
+        } catch (Exception e) {
+            System.out.println("연동실패");
+        }
+    }
+
+
+} //class end
