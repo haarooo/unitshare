85546561fdc5d3915fc4471278fb6a13f3488cb5
haarooo <pshb200137@gmail.com>
2026-02-11 15:39:24 +0900

    물품등록,조회 함수

--- src/unitshare/controller/ProductController.java
+++ src/unitshare/controller/ProductController.java
@@ -1,6 +1,9 @@
 package unitshare.controller;
 
 import unitshare.model.dao.ProductDao;
+import unitshare.model.dto.ProductDto;
+
+import java.util.ArrayList;
 
 public class ProductController {
     // 싱글톤 생성
@@ -10,4 +13,15 @@ public class ProductController {
 
     private ProductDao pd = ProductDao.getInstance();
 
+
+    //20. 물품등록
+    public boolean productAdd(String pname , int pprice , String pcontent , int people , String openchat){
+        boolean result = pd.productAdd(pname, pprice, pcontent, people, openchat);
+        return result;
+    }
+    //21. 전체 공동구매 목록조회
+    public ArrayList<ProductDto> findAll(){
+        ArrayList<ProductDto> result = pd.findAll();
+        return result;
+    }
 }
--- src/unitshare/model/dao/ProductDao.java
+++ src/unitshare/model/dao/ProductDao.java
@@ -1,11 +1,13 @@
 package unitshare.model.dao;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import unitshare.model.dto.ProductDto;
+
+import java.sql.*;
+import java.util.ArrayList;
 
 public class ProductDao {
     //싱글톤 생성
-    private ProductDao(){}
+    private ProductDao(){connect();}
     private static final ProductDao instance = new ProductDao();
     public static ProductDao getInstance() {
         return instance;
@@ -27,6 +29,43 @@ public class ProductDao {
         }
     }
 
+    //21. 물품등록
+    public boolean productAdd(String pname , int pprice , String pcontent , int people , String openchat){
+        try {
+            String sql = "insert into product(pname , pprice , pcontent , people , openchat)values(?,?,?,?,?)";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ps.setString(1, pname);
+            ps.setInt(2, pprice);
+            ps.setString(3, pcontent);
+            ps.setInt(4, people);
+            ps.setString(5, openchat);
+            int count = ps.executeUpdate();
+            if (count == 1) {return true;}
+            else {return false;}
+        }catch (SQLException e){System.out.println("[시스템오류] SQL 문법 문제발행" + e);}
+        return false;
+    }
 
+    //22. 전체 공동구매 목록조회
+    public ArrayList<ProductDto> findAll(){
+        ArrayList<ProductDto> products = new ArrayList<>();
+        try{
+            String sql = "select * from product";
+            PreparedStatement ps = conn.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                int pno = rs.getInt("pno");
+                String pname = rs.getString("pname");
+                int pprice = rs.getInt("pprice");
+                String pcontent = rs.getString("pcontent");
+                String pdate = rs.getString("pdate");
+                String openchat = rs.getString("openchat");
+                int people = rs.getInt("people");
+                ProductDto productDto = new ProductDto(pno , pname , pprice , pcontent , pdate , openchat , people);
+                products.add(productDto);
+            }
+        }catch(SQLException e){System.out.println("sql 문법문제 2" + e);}
+        return products;
+    }
 
 }
