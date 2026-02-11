275bbf2db2330f28003397c80aff4b916c4f5699
haarooo <pshb200137@gmail.com>
2026-02-11 14:38:18 +0900

    prductcontorller 싱글톤

--- src/unitshare/controller/ProductController.java
+++ src/unitshare/controller/ProductController.java
@@ -1,8 +1,13 @@
 package unitshare.controller;
 
+import unitshare.model.dao.ProductDao;
+
 public class ProductController {
     // 싱글톤 생성
     private ProductController(){}
     private static final ProductController instance = new ProductController();
     public static ProductController getInstance(){return instance;}
+
+    private ProductDao pd = ProductDao.getInstance();
+
 }
